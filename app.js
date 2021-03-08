bf00879e32c6913bb90f95d8df6ee53b925ddabd
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-03-07 21:35:45 -0500

    Template Literals Added

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ const app = new Vue({
         'Okay, I learned HTML, CSS, and JavaScript. But, how do I combine them together?? Send help.', 
         'Today I start learning Vue. I got this.'
       ],
-      bio: 'Excited future software engineer.'
+      bio: 'Excited future front-end engineer.'
     }
   });
